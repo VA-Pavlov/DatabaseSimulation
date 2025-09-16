022bc2dca0af93413df5a9aab024c75c12d70e79
teacher <teacher@tp.local>
2025-09-16 21:47:51 +0300

    Удалили метод DELETE

--- DatabaseSimulation/Query.cs
+++ DatabaseSimulation/Query.cs
@@ -19,7 +19,6 @@ namespace DatabaseSimulation
 
                     break;
                 case "DELETE":
-                    Delete(query.Split(' ')[2]);
                     break;
                 case "INSERT":
                     break;
@@ -29,38 +28,6 @@ namespace DatabaseSimulation
             }
         }
 
-        private static void Delete(string expression) //id!=34 id>5
-        {
-            
-            var sign = "";
-            var number = "";
-            for (int i = 2;i < expression.Length; i++)
-            {
-                if (char.IsDigit(expression[i]))
-                    number += expression[i];
-                else
-                    sign += expression[i];
-            }
-            switch (sign)
-            {
-                case "=":
-                    break;
-                case "!=":
-                    break;
-                case ">":
-                    break;
-                case "<":
-                    break;
-                case "<=":
-                    break;
-                case ">=":
-                    break;
-                case "like":
-                    break;
-                default:
-                    Console.WriteLine("Некорректный запрос в блоке WHERE");
-                    break;
-            }
-        }
+        
     }
 }
